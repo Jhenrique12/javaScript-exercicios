09096b421a5206be0da3754a86ad5d13a81be7b3
Jhenrique12 <jonhenrique007@gmail.com>
2022-11-09 20:35:25 -0300

    Finalizando todas as funcionalidades

--- cadastroVagasEmprego/cadastroVagasEmprego.js
+++ cadastroVagasEmprego/cadastroVagasEmprego.js
@@ -1,19 +1,3 @@
-// const listaVagas  = [] // array de objetos que vai receber cada vaga com suas especificações -> usar índice da lista
-// const vagas = {}
-// const vagas = {   // objeto vagas que vai compor a listaVagas sendo cada propriedade uma caracteristica da vaga
-//   nome,
-//   quantidadeCandidatos,
-//   descricao,
-//   dataLimite,
-//   nomesCandidatos, // array de strings com nomes de candidatos
-// }
-
-// function listarVagas () { //nome, indice, qtd cadidatos
-//   listaVagas.forEach(function(vaga, indiceVaga,){
-
-//   })
-// }
-
 const listaVagas = [];
 
 function criarVagas() {
@@ -45,9 +29,9 @@ function listarVagas() {
   if (listaVagas.length > 0) {
     listaVagas.forEach(function (vaga, indice) {
       vagas += `\n
-      Indice da Vaga ${indice + 1}
+      Indice da vaga: ${indice + 1}
       Nome da vaga: ${vaga.nome}
-      Quantidade de Candidatos ${vaga.quantidadeCandidatos}`;
+      Quantidade de Candidatos: ${vaga.quantidadeCandidatos}`;
     });
 
     alert("Lista de vagas disponíveis:" + vagas);
@@ -55,7 +39,6 @@ function listarVagas() {
     alert("Sem vagas dispovíveis!");
   }
 }
-
 function inscreverCandidato() {
   alert("Preparando candidatura...");
 
@@ -90,3 +73,107 @@ function inscreverCandidato() {
     });
   }
 }
+function visualizarVaga() {
+  alert("Preparando visualização...");
+
+  listarVagas();
+
+  let indiceVagaEscolhida = prompt(
+    "Qual o índice da vaga que quer visualizar?"
+  );
+
+  if (indiceVagaEscolhida > listaVagas.length || indiceVagaEscolhida < 1) {
+    alert("Índice inválido");
+  } else {
+    listaVagas.forEach(function (vaga, indice) {
+      if (indice + 1 == indiceVagaEscolhida) {
+        alert(`
+        Índice da vaga: ${indice + 1}
+        Nome da vaga: ${vaga.nome}
+        Descrição: 
+        ${vaga.descricao}
+        Data limite: ${vaga.dataLimite}
+        Quantidade de candidatos: ${vaga.quantidadeCandidatos}
+        Candidatos: ${vaga.nomesCandidatos}
+        `);
+      }
+    });
+  }
+}
+function excluirVaga() {
+  alert("Iniciando exclusão de vaga...");
+
+  listarVagas();
+
+  let indiceVagaEscolhida = prompt("Qual o índice da vaga que quer excluir?");
+
+  if (indiceVagaEscolhida > listaVagas.length || indiceVagaEscolhida < 1) {
+    alert("Índice inválido");
+  } else {
+    listaVagas.forEach(function (vaga, indice) {
+      if (indice + 1 == indiceVagaEscolhida) {
+        let confirmacaoVaga = confirm(`
+        Confirmar vaga a ser excluída? Ok = SIM, Cancelar = NÃO
+
+        Índice da vaga: ${indice + 1}
+        Nome da vaga: ${vaga.nome}
+        Descrição: 
+        ${vaga.descricao}
+        Data limite: ${vaga.dataLimite}
+        `);
+
+        if (confirmacaoVaga) {
+          listaVagas.splice(indiceVagaEscolhida - 1, 1);
+          alert("Exclusão feita com sucesso!");
+        } else {
+          alert("Processo de exclusão cancelado...");
+        }
+      }
+    });
+  }
+}
+
+let option = 0;
+do {
+  option = parseFloat(
+    prompt(`
+  Sistema de cadastro de vagas!
+  
+  == MENU ==
+  
+  Escolha sua opção
+  1 - Criar vaga
+  2 - Listar vagas
+  3 - Inscrever candidato
+  4 - Visualizar vaga
+  5 - Excluir vaga
+  6 - SAIR`)
+  );
+
+  switch (option) {
+    case 1:
+      criarVagas();
+      break;
+    case 2:
+      listarVagas();
+      break;
+    case 3:
+      listaVagas.length > 0
+        ? inscreverCandidato()
+        : alert("Sem vagas disponíveis! Por favor criar uma vaga.");
+      break;
+    case 4:
+      listaVagas.length > 0
+        ? visualizarVaga()
+        : alert("Sem vagas disponíveis! Por favor criar uma vaga.");
+      break;
+    case 5:
+      listaVagas.length > 0
+        ? excluirVaga()
+        : alert("Sem vagas disponíveis! Por favor criar uma vaga.");
+      break;
+    case 6:
+      alert("Saindo...");
+      break;
+  }
+} while (option !== 6);
